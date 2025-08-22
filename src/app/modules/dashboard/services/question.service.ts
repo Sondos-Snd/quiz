28dfa83ec2be496379c1afe75047a195c51ee1b8
Aouachri Sondos <awachri.sondos@gmail.com>
2025-08-22 18:33:44 +0200

    Update question.service.ts

--- src/app/modules/dashboard/services/question.service.ts
+++ src/app/modules/dashboard/services/question.service.ts
@@ -1,19 +1,27 @@
 // src/app/services/question.service.ts
-@Injectable({ providedIn: 'root' })
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
 export class QuestionService {
-  private readonly baseUrl = 'https://quiz-1-pwdb.onrender.com/api'; // <- single slash
 
-  constructor(private http: HttpClient) {}
+  private baseUrl = 'https://quiz-1-pwdb.onrender.com/api';
 
-  getQuestions(topic: string) {
-    return this.http.get(`${this.baseUrl}/questions/${encodeURIComponent(topic)}`);
+  constructor(private http: HttpClient) { }
+
+  getQuestions(topic: string): Observable<any> {
+    return this.http.get(`${this.baseUrl}/questions/${topic}`);
   }
 
-  getAllQuestions() {
+  getAllQuestions(): Observable<any> {
     return this.http.get(`${this.baseUrl}/questions`);
   }
 
-  checkAnswer(answerRequest: any) {
+  checkAnswer(answerRequest: any): Observable<any> {
     return this.http.post(`${this.baseUrl}/answers/check`, answerRequest);
   }
 }
